3abed52edca8ec359cb582e320c050029ef79fb8
Johan Mickos <jmickos@bu.edu>
2013-10-28 23:48:59 -0400

    Parallelized Item Detect Module
    Detectors can now take in multiple ROI to allow for different # player modes. In the example, all 4 players are accounted for for the item boxes. NOTES:
    * Interestingly, the algo. sped up significantly whn the ROI loop was moved to the outside. Maybe frame[x0:x1, y0:y1] is costly?
    
    ** CRITICAL: Thresholding is thrown off by red vs green shells! Maybe we should split the Manhattan distance up into the three channels?

--- 8t.py
+++ 8t.py
@@ -1,5 +1,5 @@
-import cv2 as cv
 import numpy as np
+import cv2 as cv
 import os, sys
 
 # Universal function to average and downsize an image
@@ -18,66 +18,44 @@ def pixelate(image, resolution):
 			# Calculate average
 			avg_b, avg_g, avg_r, _ = cv.mean(image[start_y:start_y+block_size[0], start_x:start_x+block_size[1]])
 			# Populate return matrix
-			rv[r][c] = [avg_r, avg_g, avg_b]
+			rv[r][c] = [avg_b, avg_g, avg_r]
 			# Debug
 			cv.rectangle(display, (block*c, block*r), (block*c+block, block*r+block), [avg_b, avg_g, avg_r], -1)
 	return rv, display
 
-# Object to represent an averaged/downsized mask
-class PixelMask:
-	def __init__(self, path, name):
-		self.name = name
-		# Open image & store pixel matrix
-		tmp = cv.imread(path)
-		self.pxl_mask, self.pxl_display = pixelate(tmp, resolution=4)
-		print '\t[PixelMask] added mask '+ self.name
-
 class Detector:
 	def __init__(self, ROI_list, masks_path, freq):
-		self.ROI = ROI_list
-		self.masks = [PixelMask(masks_path+name, name) for name in os.listdir(masks_path)]
+		self.ROI_list = ROI_list
+		self.masks = [(cv.imread(masks_path+name), name) for name in os.listdir(masks_path)]
 		self.freq = freq
-		print '[detector] initialization complete.'
 
 	def detect(self, frame, cur_count):
 		if cur_count % self.freq is 0:
 			self.process(frame, cur_count)
 
 	def process(self, frame, cur_count):
-		for mask in self.masks:
-			try:
-				regions = iter(self.ROI)
-			except:
-				# Only one ROI to check
-				y0 = self.ROI[0][0]
-				y1 = self.ROI[1][0]
-				x0 = self.ROI[0][1]
-				x1 = self.ROI[1][1]
-			else:
-				# Multiple ROI to check
-				for ROI in regions:
-					y0 = ROI[0][0]
-					y1 = ROI[1][0]
-					x0 = ROI[0][1]
-					x1 = ROI[1][1]
+		cnt = 1
+		for ROI in self.ROI_list:
+			# Extract ROI points
+			y0 = ROI[0][0]
+			y1 = ROI[1][0]
+			x0 = ROI[0][1]
+			x1 = ROI[1][1]
 
-					# Pixelate current ROI in frame
-					region = frame[x0:x1, y0:y1]
-					f_pxl, f_disp = pixelate(region, resolution=4)
-
-					# Compare for similarities
-					distance = sum(sum(sum(abs(np.int16(f_pxl) - np.int16(mask.pxl_mask)))))
-
-					if distance <= 1100:
-						print str(cur_count) + ' contains ' + mask.name
-						cv.rectangle(frame, (y0, x0), (y1, x1), (0, 255, 0))
-						cv.imshow('compared to...', mask.pxl_display)
-						c = cv.waitKey(0)
-						if c == 27:
-							exit(0)
-					cv.imshow('itembox',f_disp)
-					cv.imshow('frame',frame)
-
+			# Pixelate current ROI in frame
+			region = frame[x0:x1, y0:y1]
+			f_pxl, f_disp = pixelate(region, resolution=4)
+			for mask in self.masks:
+				# Compute Manhattan distance
+				distance = sum(sum(sum(abs(np.int16(f_pxl) - np.int16(mask[0])))))
+				if distance <= 1030:
+					print distance
+					print 'Player ' + str(cnt) + ' has ' + mask[1]
+					cv.rectangle(frame, (y0, x0), (y1, x1), (0, 255, 3))
+					c = cv.waitKey(0)
+					if c == 27:
+						exit(0)
+			cnt = cnt+1
 class Race:
 	def __init__(self, src):
 		self.name = src
@@ -98,6 +76,7 @@ class Race:
 		# Init
 		while True:
 			ret, self.cur_frame = self.capture.read()
+			cv.imshow(self.name, self.cur_frame)
 			for d in self.detectors:
 				d.detect(self.cur_frame, self.frame_cnt)
 			self.frame_cnt += 1
@@ -122,7 +101,7 @@ if __name__ == '__main__':
 							((541,34), (593, 74)),
 							((45, 264), (97, 304)),
 							((541, 264), (593, 304))],
-						masks_path='./items/',
+						masks_path='./pxl_items/',
 						freq=1)
 	r.add_detector(items_3p)
 	r.process()
