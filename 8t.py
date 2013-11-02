54b9c9b9ef7b48ecc49c402f1942fa36cd4fc3ff
Johan Mickos <jmickos@bu.edu>
2013-11-02 12:41:29 -0400

    More generic Detector class
    Parent Detector class still iterates through ROIs and masks for its own instance,
    but now passes control over to the child class's own handle() method.
    This allows for custom handling of mask matches.
    
    TODO:
    - All detectors need to have shared access to an array of Player instances.
      This allows them to update the status of each player, as well as to have some
      kind of memory associated with the changing states.
    - class ItemDetector(Detector) needs to access ^ array in order to ignore the roulette spinning
      in the itemboxes. This could be done with a circular buffer that stores the N previous frames

--- 8t.py
+++ 8t.py
@@ -23,12 +23,7 @@ def pixelate(image, resolution):
 			cv.rectangle(display, (block*c, block*r), (block*c+block, block*r+block), [avg_b, avg_g, avg_r], -1)
 	return rv, display
 
-# Universal function for normalizing an image
-def normalize(image, range):
-	local_range = arr.max()-arr.min()
-	amin = arr.min()
-	return (arr-amin)*range/local_range
-
+# Parent class for all detectors
 class Detector:
 	def __init__(self, ROI_list, masks_path, freq):
 		self.ROI_list = ROI_list
@@ -40,8 +35,8 @@ class Detector:
 			self.process(frame, cur_count)
 
 	def process(self, frame, cur_count):
-		cnt = 1
-
+		# Player counter
+		player = 1
 		# Flow control
 		x = 1
 		for ROI in self.ROI_list:
@@ -50,12 +45,11 @@ class Detector:
 			y1 = ROI[1][0]
 			x0 = ROI[0][1]
 			x1 = ROI[1][1]
-
 			# Pixelate current ROI in frame
 			region = frame[x0:x1, y0:y1]
 			f_pxl, f_disp = pixelate(region, resolution=4)
 			bf, gf, rf = cv.split(f_pxl)
-			cv.imshow('frame', f_disp)
+			#cv.imshow('frame', f_disp)
 			for mask in self.masks:
 				print mask[1]
 				bm, gm, rm = cv.split(mask[0])
@@ -64,33 +58,26 @@ class Detector:
 				dif_r = sum(sum(abs(np.int16(rm) - np.int16(rf))))
 				print dif_r, dif_g, dif_b
 				tot = dif_b+dif_g+dif_r
-				if dif_b < 450 and dif_g < 450 and dif_r < 450 and tot <= 1030:
-					print '\t\t\tPlayer ' + str(cnt) + ' has ' + mask[1]
-					c = cv.waitKey(0)
-					if c == 27:
-						exit(0)
+				if dif_b < 420 and dif_g < 420 and dif_r < 420 and tot <= 1020:
+					# Transfer control to child class
+					self.handle(frame, cur_count, player, mask)
 			c = cv.waitKey(x)
 			if c is 27:
 				exit(0)
 			elif c is 32:
 				x ^= 1
 				cv.waitKey(0)
-
-			'''
-				# Compute Manhattan distance
-				distance = sum(sum(sum(abs(np.int16(f_pxl) - np.int16(mask[0])))))
-				if distance <= 1030:
-					print distance
-					print 'Player ' + str(cnt) + ' has ' + mask[1]
-					cv.rectangle(frame, (y0, x0), (y1, x1), (0, 255, 3))
-					c = cv.waitKey(0)
-					if c == 27:
-						exit(0)
-			'''
-			cnt = cnt+1
-
+			player += 1
+''' SPECIFIC detectors
+* ItemDetector: handles itemboxes for all players
+'''
 class ItemDetector(Detector):
-	
+	def handle(self, frame, cur_count, player, mask):
+		print '\t\t\tPlayer ' + str(player) + ' has ' + mask[1]
+		c = cv.waitKey(0)
+		if c == 27:
+			exit(0)
+
 class Race:
 	def __init__(self, src):
 		self.name = src
@@ -113,6 +100,7 @@ class Race:
 			print 'NEWFRAME------------'
 			ret, self.cur_frame = self.capture.read()
 			cv.imshow(self.name, self.cur_frame)
+			cv.waitKey(1)
 			for d in self.detectors:
 				d.detect(self.cur_frame, self.frame_cnt)
 			self.frame_cnt += 1
@@ -126,11 +114,11 @@ if __name__ == '__main__':
 	print '\t<space> pauses/unpauses current frame\n\n'
 
 	r = Race(sys.argv[1])
-	items_3p = Detector(ROI_list=[
-							((45, 34), (97, 74))],
-							#((541,34), (593, 74)),
-							#((45, 264), (97, 304)),
-							#((541, 264), (593, 304))],
+	items_3p = ItemDetector(ROI_list=[
+							((45, 34), (97, 74)),
+							((541,34), (593, 74)),
+							((45, 264), (97, 304)),
+							((541, 264), (593, 304))],
 						masks_path='./pxl_items/',
 						freq=1)
 	r.add_detector(items_3p)
