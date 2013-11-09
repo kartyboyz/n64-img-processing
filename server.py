44c500c38f1e5f0e7674c386c841470dff50fe9b
Johan Mickos <jmickos@bu.edu>
2013-11-09 15:26:26 -0500

    Database communication
    Initial commit for sending/receiving session and race data between processing and database.
    server.py:
    	get_session() - Returns data concerning one (or all) sessions
    	get_races()   - Returns data conerning all races in a given session
    	put_race()    - Adds a new race in database with start time & duration within a session

---
+++ server.py
@@ -0,0 +1,43 @@
+import requests
+import json
+
+# System defaults. Will be overrideable at a later time
+server = 'http://localhost'
+port = 5000
+
+def get_session(session_id):
+    '''
+    Communicates with database to extract information for a given session.
+    If session_id == 0, returns all sessions in database
+    '''
+    global server, port
+    session = '/sessions'
+    if session_id is not 0:
+        session += '/' + str(session_id)
+    payload = '%s:%d%s'%(server, port, session)
+    response = requests.get(payload)
+    print response.text
+    return response
+
+def get_races(session_id):
+    '''
+    Communicates with database to extract information about races in session
+    '''
+    global server, port
+    path = '/sessions/%d/races'%(session_id)
+    payload = '%s:%d%s'%(server, port, path)
+    response = requests.get(payload)
+    print response.text
+    return response
+
+def put_race(session_id, start_time, duration):
+    '''
+    Sends race JSON object to database for storage.
+    '''
+    global server, port
+    url = '%s:%d/sessions/%d/races'%(server, port, session_id)
+    payload = {'start_time' : start_time, 'duration' : duration}
+    headers = {'content-type': 'application/json'}
+    response = requests.post(url, data=json.dumps(payload), headers=headers)
+    print response.text
+    return response
