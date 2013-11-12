bd764e93dcf4798532995867af80424c1ff09969
Johan Mickos <jmickos@bu.edu>
2013-11-12 18:24:38 -0500

    Multiprocessing (not threading) + bugfix

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask
 import json
 import os
 import threading
+import multiprocessing
 import urllib2
 
 import phase_0
@@ -12,6 +13,8 @@ import tempfile
 port = 5001
 
 app = Flask(__name__)
+app.debug = True
+
 def download_race(video_url):
     f = tempfile.NamedTemporaryFile()
     chunk_size = 4 * 1024 * 1024
@@ -30,21 +33,20 @@ def download_race(video_url):
         print "Couldn't download ", video_url
         return None
 
-def find_races(session_id, video_file):
+def find_races(session_id, video_url):
+    video_file = download_race(video_url)
+    # Download movie
+    if video_file is None:
+        return -1
     phase_0.main(session_id, video_file)
 
-@app.route('/race_detection/<int:session_id>')
+@app.route('/race_detection/<int:session_id>', methods=['POST'])
 def rcv_session_id(session_id):
     # Fetch URL
     session = database.get_session(session_id)
     video_url = session.json()[u'video_url']
-    # Download movie
-    video_file = download_race(video_url)
-    if video_file is None:
-        return -1
     # Pass control to processing thread
-    # XXX: Limit total threads?
-    phase_0_thread = threading.Thread(target = find_races, args=(session_id, video_file))
+    phase_0_thread = multiprocessing.Process(target = find_races, args=(session_id, video_url))
     phase_0_thread.start()
     return 'Launched job for session ID %d\n' % session_id
 
