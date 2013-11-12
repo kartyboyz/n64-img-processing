156109446641a6a196a46f6d599a8812e3c2a649
Michael Abed <michaelabed@gmail.com>
2013-11-12 18:15:42 -0500

    Fix server interactions with other servers

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import json
 import os
 import threading
 import urllib2
+import requests
 
 import phase_0
 import database
@@ -32,8 +33,9 @@ def download_race(video_url):
 
 def find_races(session_id, video_file):
     phase_0.main(session_id, video_file)
+    requests.post('http://localhost:5002/video_done/%d'% session_id)
 
-@app.route('/race_detection/<int:session_id>')
+@app.route('/race_detection/<int:session_id>', methods=['POST'])
 def rcv_session_id(session_id):
     # Fetch URL
     session = database.get_session(session_id)
@@ -44,6 +46,7 @@ def rcv_session_id(session_id):
         return -1
     # Pass control to processing thread
     # XXX: Limit total threads?
+    #find_races(session_id, video_file)
     phase_0_thread = threading.Thread(target = find_races, args=(session_id, video_file))
     phase_0_thread.start()
     return 'Launched job for session ID %d\n' % session_id
