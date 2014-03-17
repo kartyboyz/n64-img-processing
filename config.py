fee253bb2f966832d564ca1def4665cd88abb8b8
Johan Mickos <jmickos@bu.edu>
2014-03-16 21:43:13 -0400

    Moved DEBUG_LEVEL to config.py

--- config.py
+++ config.py
@@ -1,3 +1,11 @@
+"""DEBUG_LEVEL :Describes intensity of feedback from video processing
+    = 0     No feedback
+    = 1     Minor feedback      Displaying current frame, print object detections, etc
+    = 2     Verbose feedback    Output intermediate values for more severe debugging
+    = 3     More verbose        This level will most likely just be used in development
+                                of features with unknown results
+"""
+DEBUG_LEVEL = 0
 
 class Race(object):
     """Race state variables
@@ -19,7 +27,7 @@ class Race(object):
             'race_duration': 0,
             'frame_rate':    0,}
         self.is_started = False
-        self.player_boxes = list();
+        self.player_boxes = list()
         self.is_black = False
 
     def reset(self):
